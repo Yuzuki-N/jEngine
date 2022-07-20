3afa23718cb48286b8c34b5dd4c0da899de68638
yuzuki <nanami2962@gmail.com>
2022-07-20 19:02:48 +0800

    draw with ebo

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -17,13 +17,16 @@ public class MainGameLoop {
                 -0.5f, 0.5f, 0f,
                 -0.5f, -0.5f, 0f,
                 0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
                 0.5f, 0.5f, 0f,
-                -0.5f, 0.5f, 0f
+        };
+
+        int[] indices = {
+                0, 1, 3,
+                3, 1, 2
         };
 
         // vao and vbo
-        RawModel model = loader.loadToVAO(vertices);
+        RawModel model = loader.loadToVAO(vertices, indices);
 
         while(!Display.isCloseRequested()) {
             renderer.prepare();
